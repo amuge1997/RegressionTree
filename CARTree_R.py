fac7dd32e3dabbda2933be3f84b776c7220b9ef8
Amuge <amuge1997z@qq.com>
2020-01-29 20:01:13 +0800

    增加注释;修改了bug使得可以处理某特征的相同的值;

--- CARTree_R.py
+++ CARTree_R.py
@@ -25,7 +25,9 @@ class CARTree_R:
             return True
         return False
 
+    # 创建节点
     def node(self,node_parent,arr_X,arr_Y):
+
         # 预测值
         fl_pre = np.mean(arr_Y)
         node_parent.set_pre(fl_pre)
@@ -38,7 +40,7 @@ class CARTree_R:
         # 计算分割点
         # 对每个特征计算最优分割点,对每个特征的分割点求最优分割点
         it_feaIdx,fl_split = self.find_best_spilt(arr_X,arr_Y)
-        if it_feaIdx is None:
+        if it_feaIdx is None:               # 样本无法划分,即特征相同,将该节点作为叶子节点,停止分裂
             # print('结束')
             node_parent.set_leaf()
             return
@@ -63,41 +65,36 @@ class CARTree_R:
         node_parent.set_righ(node_righ)
         self.node(node_righ,arr_righX,arr_righY)
 
+    # 找到最优分割特征和分割点
     def find_best_spilt(self,arr_X,arr_Y):
         # 输入 arr_X,arr_Y
-
-        arr_minSplit = np.zeros((self.it_feaSum))
-        arr_minError = np.zeros((self.it_feaSum))
+        fl_minSplit = None
+        it_minFea = None
         for it_feaidx in range(self.it_feaSum):
             fl_split,fl_error = self.find_best_spilt_from_fea(it_feaidx,arr_X,arr_Y)
-            arr_minSplit[it_feaidx] = fl_split
-            arr_minError[it_feaidx] = fl_error
-
-        it_minErrorIdx = np.argmin(arr_minError)
-        it_minFea = it_minErrorIdx
-        fl_split = arr_minSplit[it_minFea]
-
-        fl_max = np.max(arr_X[it_minFea])
-        fl_min = np.min(arr_X[it_minFea])
-
-        if fl_split == fl_max or fl_split == fl_min:
-            it_minFea = None
-            fl_split = None
+            if it_minFea is None and fl_split is not None:
+                fl_minSplit = fl_split
+                it_minFea = it_feaidx
+            if fl_minSplit is not None and fl_split<=fl_minSplit:
+                fl_minSplit = fl_split
+                it_minFea = it_feaidx
 
         # 返回 it_feature,fl_split
-        return it_minFea,fl_split
+        return it_minFea,fl_minSplit
 
+    # 从一个特征中找到最优分割点
     def find_best_spilt_from_fea(self,it_feaidx,arr_X,arr_Y):
         arr_feaX = arr_X[it_feaidx,:]
         arr_sortX = np.sort(arr_feaX)
         arr_unqiX = np.unique(arr_sortX)
 
+        if arr_unqiX.shape[0] == 1:
+            return None,None
+
         arr_temp1 = arr_unqiX[1:]
         arr_temp2 = arr_unqiX[:-1]
         arr_temp3 = (arr_temp1 + arr_temp2)/2
         arr_split = arr_temp3
-        # arr_temp4 = np.append([arr_unqiX[0]],arr_temp3)
-        # arr_split = np.append(arr_temp4,[arr_unqiX[-1]])
         arr_bestSplit = np.zeros((arr_split.shape))
 
         for it_idx,fl_split in enumerate(arr_split):
@@ -115,6 +112,7 @@ class CARTree_R:
         # 返回 fl_split
         return fl_minSplit,fl_minError
 
+    # 方差计算
     def cal_error(self,arr_Y1,arr_Y2):
         if arr_Y1.shape[0] == 0:
             fl_error = np.var(arr_Y2)
@@ -124,6 +122,7 @@ class CARTree_R:
             fl_error = np.var(arr_Y1) + np.var(arr_Y2)
         return fl_error
 
+    # 预测
     def predict(self,arr_X):
         def pre(node,arr_xt):
             if not node.bl_leaf:
@@ -175,10 +174,10 @@ if __name__ == '__main__':
     # ])
 
     arr_X = np.array([
-        [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        [0, 0, 1, 2, 3, 4, 5, 6, 7, 8]
     ]).T
     arr_Y = np.array([
-        [1, 0.5, 1, 2, 2.5, 2, 4, 4.5, 4]
+        [1, 1.1, 0.5, 1, 2, 2.5, 2, 4, 4.5, 4]
     ]).T
 
     arr_Xt = np.linspace(-1,10,100).reshape(-1,1)
